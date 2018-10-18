128fb9e3669b4092f92859d1f96eaed17b012474
Hoàng Văn Việt Anh <hoangvanvietanh@gmail.com>
2018-10-18 17:00:07 +0700

    Good job

--- src/main/java/com/java/vakapu/controller/AccountController.java
+++ src/main/java/com/java/vakapu/controller/AccountController.java
@@ -12,7 +12,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -20,7 +19,6 @@ import com.java.vakapu.entity.Account;
 import com.java.vakapu.model.AccountModel;
 import com.java.vakapu.services.AccountServices;
 import com.java.vakapu.services.EmailServices;
-import com.java.vakapu.services.ProfileServices;
 
 import utils.RandomPassword;
 
@@ -34,16 +32,13 @@ public class AccountController {
 	@Autowired
 	private EmailServices emailServices;
 
-	@Autowired
-	private ProfileServices profileServices;
-	
 	@RequestMapping(method = RequestMethod.GET)
 	public String create(Model model) {
 		AccountModel account = new AccountModel();
 		model.addAttribute("account", account);
 		return "redirect:/account";
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
 	public String handleCreate(@ModelAttribute("email") String emailSignUp, BindingResult result, ModelMap model,
 			RedirectAttributes redirectAttributes) throws ParseException {
@@ -59,7 +54,7 @@ public class AccountController {
 				BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 				String code = random.getCode();
 				model.addAttribute("code", code);
-				Account acc= accountServices.findByEmail(emailSignUp);
+				Account acc = accountServices.findByEmail(emailSignUp);
 				emailServices.sendSimpleMessage(emailSignUp, code);
 				acc.setPassword(passwordEncoder.encode(code));
 				accountServices.updateAccount(acc);
@@ -69,67 +64,54 @@ public class AccountController {
 		return "redirect:/forgot-password";
 	}
 
-
 	@RequestMapping(value = "/authentication", method = RequestMethod.GET)
 	public String checkCode(@ModelAttribute("email") String email, Model model) {
-		
+
 		return "redirect:/sign-in";
 	}
-	
 
-	@RequestMapping(value="/change-password",method=RequestMethod.GET)
-	public String changePasswordGet(ModelMap model, Model mode)
-	{
-		String current= accountServices.getEmailUser();
+	@RequestMapping(value = "/change-password", method = RequestMethod.GET)
+	public String changePasswordGet(ModelMap model, Model mode) {
+		String current = accountServices.getEmailUser();
 		model.put("email", current);
 		System.out.println(current);
-		
-		Account acc= accountServices.findByEmail(current);
-		AccountModel account= new AccountModel();
+
+		Account acc = accountServices.findByEmail(current);
+		AccountModel account = new AccountModel();
 		account.fromAccount(acc);
 		mode.addAttribute("account", account);
 		return "change-password";
-	}		
-		
-	
-	
-	@RequestMapping(value="/change-password",method=RequestMethod.POST)
-	public String changePasswordPost(@ModelAttribute("account") AccountModel account, @ModelAttribute("oldpass") String oldPass, 
-			@ModelAttribute("oldPass") String oldPassword,
-			@ModelAttribute("newpass") String newPass,@ModelAttribute("newre_pass") String newRePass ,
-			BindingResult result, ModelMap model,
-			RedirectAttributes redirectAttributes) throws ParseException
-	{
-		if(result.hasErrors())
-		{
+	}
+
+	@RequestMapping(value = "/change-password", method = RequestMethod.POST)
+	public String changePasswordPost(@ModelAttribute("account") AccountModel account,
+			@ModelAttribute("oldpass") String oldPass, @ModelAttribute("oldPass") String oldPassword,
+			@ModelAttribute("newpass") String newPass, @ModelAttribute("newre_pass") String newRePass,
+			BindingResult result, ModelMap model, RedirectAttributes redirectAttributes) throws ParseException {
+		if (result.hasErrors()) {
 			return "redirect:/forgot-password/change-password";
 		}
-		if(oldPass.equals(oldPassword))
-		BCryptPasswordEncoder Encoder=new BCryptPasswordEncoder();
-		String pass=Encoder.encode(oldPass);
-		if(oldPass.equals(oldPassword))
-		{
-			if(newPass.equals(newRePass))
-			{
-				Account acc=account.toAccount();
-				//accountServices.deletePassword(acc);
-				
-				BCryptPasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
-				acc.setPassword(passwordEncoder.encode(newRePass));
-				acc.setStatus("active");
-				accountServices.updateAccount(acc);
+		if (oldPass.equals(oldPassword)) {
+			if (oldPass.equals(oldPassword)) {
+				if (newPass.equals(newRePass)) {
+					Account acc = account.toAccount();
+					// accountServices.deletePassword(acc);
+
+					BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+					acc.setPassword(passwordEncoder.encode(newRePass));
+					acc.setStatus("active");
+					accountServices.updateAccount(acc);
+				}
 			}
 		}
-		
-			
+
 		return "redirect:/sign-in";
 	}
-	
-	@RequestMapping(value="/deactivate",method=RequestMethod.GET)
-	public String  deactivate(ModelMap model)
-	{
-		String current=accountServices.getEmailUser();
-		Account a=accountServices.findByEmail(current);
+
+	@RequestMapping(value = "/deactivate", method = RequestMethod.GET)
+	public String deactivate(ModelMap model) {
+		String current = accountServices.getEmailUser();
+		Account a = accountServices.findByEmail(current);
 		a.setStatus("deactive");
 		accountServices.updateAccount(a);
 		return "redirect:/profile";
--- src/main/java/com/java/vakapu/controller/ForgotPasswordController.java
+++ src/main/java/com/java/vakapu/controller/ForgotPasswordController.java
@@ -1,28 +1,31 @@
 package com.java.vakapu.controller;
 
 
-<<<<<<< HEAD
-=======
-import javax.jws.WebParam.Mode;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
->>>>>>> branch 'master' of https://github.com/hoangvanvietanh/WebWorkingGroupsMVC.git
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.google.protobuf.TextFormat.ParseException;
+import com.java.vakapu.entity.Account;
 import com.java.vakapu.model.AccountModel;
+import com.java.vakapu.services.AccountServices;
 
 @Controller
 
 @RequestMapping("/forgot-password")
 public class ForgotPasswordController {
 	
+	@Autowired
+	private AccountServices accountServices;
 	@RequestMapping(method = RequestMethod.GET)
 	public String homeForgot(Model model) {
 //		AccountModel account = new AccountModel();
