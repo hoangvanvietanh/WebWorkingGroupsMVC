bea6c5dd10884e0618d6aab58451d8ce5e2fcfc6
Hoàng Văn Việt Anh <hoangvanvietanh@gmail.com>
2018-10-30 14:50:15 +0700

    completed send email invited friend join to team

--- src/main/java/utils/RandomCode.java
+++ src/main/java/utils/RandomCode.java
@@ -8,7 +8,7 @@ public class RandomCode {
 		String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
         StringBuilder salt = new StringBuilder();
         Random rnd = new Random();
-        while (salt.length() < 5) { 
+        while (salt.length() < 4) { 
             int index = (int) (rnd.nextFloat() * SALTCHARS.length());
             salt.append(SALTCHARS.charAt(index));
         }
